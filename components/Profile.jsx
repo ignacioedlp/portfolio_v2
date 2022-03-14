654678fd35841e26844979ce1ed49c8b2238eda3
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2022-03-14 12:02:19 -0300

    fix ui v1.0.0

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,14 +5,14 @@ function Profile({ lenguage }) {
   return (
     <section className="text-gray-400 bg-gray-900 body-font ">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="animate__animated animate__backInLeft  lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center ">
-          <h1 className="title-font  sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <div className="  lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center ">
+          <h1 className=" animate__animated animate__backInLeft title-font  sm:text-4xl text-3xl mb-4 font-medium text-white">
             {lenguage == "es"
               ? "Hola soy Ignacio, Desarrollador FullStack Jr."
               : "Hello I'm Ignacio, Junior FullStack Developer"}
           </h1>
 
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8 leading-relaxed animate__animated animate__backInLeft">
             {lenguage == "es"
               ? "Con capacidad de pensar nuevas ideas e implementar mejoras. Soy detallista y ordenado con los proyectos queme propongo"
               : "With the ability to think new ideas and implement improvements. I am detail oriented and organized with the projects I propose"}
@@ -20,22 +20,22 @@ function Profile({ lenguage }) {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className=" animate__animated  animate__shakeY inline-flex text-white bg-red-700 border-0 py-2 px-6  focus:outline-none hover:bg-red-600 rounded text-lg font-medium"
+              className=" animate__animated animate__backInLeft inline-flex text-white bg-red-700 border-0 py-2 px-6  focus:outline-none hover:bg-red-600 rounded text-lg font-medium"
             >
               {lenguage == "es" ? "Contactame" : "Contact me"}
             </a>
             <a
               download={lenguage == "es" ? "SpanishCv.pdf" : "EnglishCV.pdf"}
               href={lenguage == "es" ? "SpanishCv.pdf" : "EnglishCV.pdf"}
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              className=" animate__animated animate__backInLeft ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
             >
               {lenguage == "es" ? "Descargar CV" : "Download CV"}
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 animate__animated animate__backInRight">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded "
+            className="object-cover object-center rounded  animate__animated animate__backInRight"
             alt="hero"
             src="/miFoto.webp"
             height={500}
