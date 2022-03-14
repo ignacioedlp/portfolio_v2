2ba5578afcbc36592a3853233aec5b63d043fcb1
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2022-03-14 11:27:30 -0300

    pwa v1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io", "upload.wikimedia.org"],
     formats: ["image/webp"],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
